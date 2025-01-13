91d86a74934b2fd3bfe42598991687f0fdeee3b2
iwasemo <iwasemo@bc.edu>
2025-01-13 18:36:19 -0500

    Initial Commit

---
+++ BoogieWonderland/BoogieWonderlandApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoogieWonderlandApp.swift
+//  BoogieWonderland
+//
+//  Created by Momoka Iwase on 2025/01/13.
+//
+
+import SwiftUI
+
+@main
+struct BoogieWonderlandApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
